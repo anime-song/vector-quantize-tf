11fb12b1f23bc2c04699d3965289b9896223db5d
anime-song <52844453+anime-song@users.noreply.github.com>
2024-02-14 18:20:49 +0900

    バージョンアップ

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="anime-song",
     description="tensorflowでの残差ベクトル量子化の実装",
     license="MIT license",
-    version="0.0.8",
+    version="0.0.9",
     install_requires=["tensorflow", "scikit-learn"],
     packages=["vector_quantize_tf"]
 )
